6be7dc66d5297d970e9f34f33becd2ec6873bb23
Deondre English <deondreenglish45@gmail.com>
2022-06-16 12:15:45 -0400

    Added Triangle test to the main window, make sure to test on windows

--- src/Application.cpp
+++ src/Application.cpp
@@ -3,6 +3,37 @@
 int main(void)
 {
     GLFWwindow* window;
+    // this will identitfy our vertex buffer
+    GLuint vertexBuffer;
+
+    static const GLfloat g_vertex_buffer_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f,  1.0f, 0.0f,
+    };
+
+    glGenBuffers(1, &vertexBuffer);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+
+    glVertexAttribPointer(
+            0,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            (void*)nullptr
+            );
+
+    // Draw the triangle
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(0);
 
     /* Initialize the library */
     if (!glfwInit())
@@ -23,7 +54,8 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        // glClear(GL_COLOR_BUFFER_BIT);
+
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
